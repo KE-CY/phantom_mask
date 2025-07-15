0e796f92c14d51a0eb57ffc1b848f062d68d8619
KE-CY <aaa01452>
2025-07-15 23:55:12 +0800

    feat: add typeorm config.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { AppDataSource } from './config/typeorm-config';
 
 import healthRoutes from './routes/healthRoute';
+import logger from './utils/logger';
 
 const app = express();
+
+// Initialize TypeORM
+AppDataSource.initialize()
+  .then(() => {
+    logger.info({ msg: 'TypeORM: Data Source has been initialized!' });
+  })
+  .catch((err) => {
+    logger.error({ msg: `TypeORM: Error during Data Source initialization: ${err.message}`, param: { stack: err.stack } });
+  });
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true, limit: '1tb' }));
