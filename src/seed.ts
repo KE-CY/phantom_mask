c7c5f820cc0d582583d0444cf0530097a9886993
KE-CY <aaa01452>
2025-07-16 16:18:48 +0800

    feat:add transaction feat.

--- src/seed.ts
+++ src/seed.ts
@@ -16,90 +16,102 @@ export const seed = async () => {
   logger.info({ msg: 'Seeding start' });
   await AppDataSource.initialize();
 
-  const pharmacyRepo = AppDataSource.getRepository(Pharmacy);
-  const maskRepo = AppDataSource.getRepository(Mask);
-  const pharmacyMaskRepo = AppDataSource.getRepository(PharmacyMask);
-  const openingHourRepo = AppDataSource.getRepository(PharmacyOpeningHour);
-  const userRepo = AppDataSource.getRepository(User);
-  const purchaseRepo = AppDataSource.getRepository(PurchaseHistory);
+  await AppDataSource.transaction(async (manager) => {
+    const pharmacyRepo = manager.getRepository(Pharmacy);
+    const maskRepo = manager.getRepository(Mask);
+    const pharmacyMaskRepo = manager.getRepository(PharmacyMask);
+    const openingHourRepo = manager.getRepository(PharmacyOpeningHour);
+    const userRepo = manager.getRepository(User);
+    const purchaseRepo = manager.getRepository(PurchaseHistory);
 
-  const maskMap = new Map<string, Mask>();
-  const pharmacyMap = new Map<string, Pharmacy>();
-  logger.debug({ msg: 'Prepare to process pharmacies.json data', pharmacySize: pharmacies.length });
-  for (const pharmacy of pharmacies) {
-    const { name, cashBalance, openingHours, masks } = pharmacy;
+    // Check if the data has been added
+    const existingCount = await userRepo.count();
+    if (existingCount > 0) {
+      logger.info({ msg: 'Data already exist in the database. Skipping seeding.' });
+      return;
+    }
 
-    // Create Pharmacy
-    const createdPharmacy = await pharmacyRepo.save(pharmacyRepo.create({
-      name,
-      cashBalance
-    }));
+    const maskMap = new Map<string, Mask>();
+    const pharmacyMap = new Map<string, Pharmacy>();
+    logger.debug({ msg: 'Prepare to process pharmacies.json data', pharmacySize: pharmacies.length });
 
-    pharmacyMap.set(name, createdPharmacy);
+    for (const pharmacy of pharmacies) {
+      const { name, cashBalance, openingHours, masks } = pharmacy;
 
-    // Create Pharmacy Opening Hours
-    const parsedHours = parseOpeningHours(openingHours);
-    const pharmacyOpeningHours = parsedHours.map(obj => ({
-      ...obj,
-      pharmacy: createdPharmacy,
-    }));
-    await openingHourRepo.save(openingHourRepo.create(pharmacyOpeningHours));
+      // Create Pharmacy
+      const createdPharmacy = await pharmacyRepo.save(pharmacyRepo.create({
+        name,
+        cashBalance
+      }));
+      pharmacyMap.set(name, createdPharmacy);
 
-    // Create Mask And PharmacyMask
-    for (const mask of masks) {
-      const { name: maskName, price } = mask;
-      let existMask = maskMap.get(maskName);
-
-      let currentMask;
-      if (!existMask) {
-        currentMask = await maskRepo.save(maskRepo.create({ name: maskName }));
-        maskMap.set(maskName, currentMask);
-      } else {
-        currentMask = existMask;
-      }
-
-      await pharmacyMaskRepo.save(pharmacyMaskRepo.create({
+      // Create Pharmacy Opening Hours
+      const parsedHours = parseOpeningHours(openingHours);
+      const pharmacyOpeningHours = parsedHours.map(obj => ({
+        ...obj,
         pharmacy: createdPharmacy,
-        mask: currentMask,
-        price,
       }));
-    }
-  }
-  logger.debug({ msg: 'Process pharmacies.json data done' });
+      await openingHourRepo.save(openingHourRepo.create(pharmacyOpeningHours));
 
-  logger.debug({ msg: 'Prepare to process users.json data', userSize: users.length });
-  for (const user of users) {
-    const { name, cashBalance, purchaseHistories } = user;
+      // Create Mask And PharmacyMask
+      for (const mask of masks) {
+        const { name: maskName, price } = mask;
+        let existMask = maskMap.get(maskName);
 
-    // Create User
-    const createdUser = await userRepo.save(userRepo.create({
-      name,
-      cashBalance
-    }));
+        let currentMask;
+        if (!existMask) {
+          currentMask = await maskRepo.save(maskRepo.create({ name: maskName }));
+          maskMap.set(maskName, currentMask);
+        } else {
+          currentMask = existMask;
+        }
 
-    // Create PurchaseHistory
-    for (const purchaseHistory of purchaseHistories) {
-      const { pharmacyName, maskName, transactionAmount, transactionDate } = purchaseHistory
-      const pharmacy = pharmacyMap.get(pharmacyName);
-      const mask = maskMap.get(maskName);
-      if (!pharmacy || !mask) {
-        logger.warn({ msg: 'Not found pharmacy or mask.' })
-        continue;
+        await pharmacyMaskRepo.save(pharmacyMaskRepo.create({
+          pharmacy: createdPharmacy,
+          mask: currentMask,
+          price,
+        }));
       }
-      await purchaseRepo.insert(purchaseRepo.create({
-        user: createdUser,
-        pharmacy,
-        mask,
-        transactionAmount,
-        transactionDate: new Date(transactionDate),
-      }));
     }
-  }
-  logger.debug({ msg: 'Process users.json data done' });
+
+    logger.debug({ msg: 'Process pharmacies.json data done' });
+
+    logger.debug({ msg: 'Prepare to process users.json data', userSize: users.length });
+    for (const user of users) {
+      const { name, cashBalance, purchaseHistories } = user;
+
+      // Create User
+      const createdUser = await userRepo.save(userRepo.create({
+        name,
+        cashBalance
+      }));
+
+      for (const purchaseHistory of purchaseHistories) {
+        const { pharmacyName, maskName, transactionAmount, transactionDate } = purchaseHistory;
+        const pharmacy = pharmacyMap.get(pharmacyName);
+        const mask = maskMap.get(maskName);
+        if (!pharmacy || !mask) {
+          logger.warn({ msg: 'Not found pharmacy or mask.' });
+          continue;
+        }
+
+        // Create PurchaseHistory
+        await purchaseRepo.insert(purchaseRepo.create({
+          user: createdUser,
+          pharmacy,
+          mask,
+          transactionAmount,
+          transactionDate: new Date(transactionDate),
+        }));
+      }
+    }
+    logger.debug({ msg: 'Process users.json data done' });
+  });
 
   logger.info({ msg: 'Seeding completed' });
   process.exit(0);
-}
+};
+
 
 const dayMap: Record<string, number> = {
   Sun: 0,
